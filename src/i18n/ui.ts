cc25876290de0e0e4df947d092bec042c2c22262
Mathis Verstrepen <mathis.verstrepen.etu@univ-lille.fr>
2023-07-30 19:46:47 +0200

    start formation page

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -25,10 +25,46 @@ export const languages = {
         'more.title': 'Learn more about',
         'more.icon.1': 'school',
         'more.text.1': 'Tech Stack & Academic career',
+        'more.url.1': 'formation',
         'more.icon.2': 'briefcase',
         'more.text.2': 'Professional experience & Testimonies ',
+        'more.url.2': 'experience',
         'more.icon.3': 'notes',
         'more.text.3': 'Projects & Portfolio',
+        'more.url.3': 'projects',
+
+        'formation.academic.title': 'Academic career',
+
+        'formation.academic.year.1': '2022-...',
+        'formation.academic.place.1': 'Polytech Lille',
+        'formation.academic.sub.1': 'Computer Science and Statistics training ',
+        'formation.academic.desc.1': 'in training',
+        'formation.academic.link': 'https://www.polytech-lille.fr/formation/8-specialites/informatique-et-statistique/',
+
+        'formation.academic.year.2': '2020-2022',
+        'formation.academic.place.2': 'Polytech Lille',
+        'formation.academic.sub.2': 'Preparatory cycle',
+        'formation.academic.desc.2': '<li>First year: <b>High Honors</b></li><li>Second year: <b>Honors</b></li>',
+
+        'formation.academic.year.3': '2017-2020',
+        'formation.academic.place.3': 'Saint Luc Cambrai',
+        'formation.academic.sub.3': 'Private Highschool',
+        'formation.academic.desc.3': '<li>“Baccalauréat” with <b>highest honors*</b> <br><small>*US equivalent : top 10% GPA <br>*UK equivalent : A or A* in A-Level examinations</small></li><li>Cambridge English B1 Preliminary passed</li>',
+    
+        'formation.lang.title': 'Language levels',
+
+        'formation.lang.1': 'French',
+        'formation.lang.1.icon': 'flag-france',
+        'formation.lang.1.desc1': 'Native language - <b>C2</b> CECRL level', 
+
+        'formation.lang.2': 'English',
+        'formation.lang.2.icon': 'flag-united-kingdom',
+        'formation.lang.2.desc1': 'CECRL level : <b>C1</b>',
+        'formation.lang.2.desc2': '<li>Highest TOEIC : 945</li><li>Cambridge English B1 Preliminary passed</li>',
+
+        'formation.lang.3': 'German',
+        'formation.lang.3.icon': 'flag-germany',
+        'formation.lang.3.desc1': 'CECRL level : <b>A1</b>',
     },
     fr: {
         'home.title' : 'Portfolio - Verstrepen Mathis',
@@ -49,9 +85,45 @@ export const languages = {
         'more.title': 'En apprendre plus',
         'more.icon.1': 'school',
         'more.text.1': 'Parcours académique & Technologies',
+        'more.url.1': 'formation',
         'more.icon.2': 'briefcase',
         'more.text.2': 'Expérience professionnelle & Témoignages',
+        'more.url.2': 'experience',
         'more.icon.3': 'notes',
         'more.text.3': 'Projets & Portfolio',
+        'more.url.3': 'projects',
+
+        'formation.academic.title': 'Parcours académique',
+
+        'formation.academic.year.1': '2022-...',
+        'formation.academic.place.1': 'Polytech Lille',
+        'formation.academic.sub.1': 'Informatique et Statistiques',
+        'formation.academic.desc.1': 'en cours',
+        'formation.academic.link': 'https://www.polytech-lille.fr/formation/8-specialites/informatique-et-statistique/',
+
+        'formation.academic.year.2': '2020-2022',
+        'formation.academic.place.2': 'Polytech Lille',
+        'formation.academic.sub.2': 'Cycle préparatoire',
+        'formation.academic.desc.2': '<li>Première année : <b>Mention Bien</b></li><li>Deuxième année : <b>Mention Assez Bien</b></li>',
+
+        'formation.academic.year.3': '2017-2020',
+        'formation.academic.place.3': 'Saint Luc Cambrai',
+        'formation.academic.sub.3': 'Lycée privé',
+        'formation.academic.desc.3': '<li>Baccalauréat avec <b>mention Très Bien</b></li><li>Cambridge English B1 Preliminary obtenu</li>',
+
+        'formation.lang.title': 'Niveaux de langues',
+
+        'formation.lang.1': 'Français',
+        'formation.lang.1.icon': 'flag-france',
+        'formation.lang.1.desc1': 'Langue maternelle - Niveau <b>C2</b> CECRL',
+
+        'formation.lang.2': 'Anglais',
+        'formation.lang.2.icon': 'flag-united-kingdom',
+        'formation.lang.2.desc1': 'Niveau CECRL : <b>C1</b>',
+        'formation.lang.2.desc2': '<li>TOEIC le plus élevé : 945</li><li>Cambridge English B1 Preliminary obtenu</li>',
+
+        'formation.lang.3': 'Allemand',
+        'formation.lang.3.icon': 'flag-germany',
+        'formation.lang.3.desc1': 'Niveau CECRL : <b>A1</b>',
     },
   } as const;
