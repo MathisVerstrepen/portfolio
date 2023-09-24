62ea28c5da5015b0181b28e3d014d603cbe3e608
Mathis Verstrepen <mathis.verstrepen.etu@univ-lille.fr>
2023-09-24 15:53:32 +0200

    ✨ Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,21 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import compressor from "astro-compressor";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), compressor()],
+  integrations: [tailwind(), compressor(), sitemap(
+    {
+        i18n: {
+          defaultLocale: 'en',
+          locales: {
+            en: 'en-US',
+            fr: 'fr-CA',
+          },
+        },
+      }
+  )],
+  site: "https://portfolio.diikstra.fr/"
 });
